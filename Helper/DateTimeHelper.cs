8118a4ee8ad8e30a9916b3c15f257888002e4cfe
arekjg <arekjg@gmail.com>
2023-12-27 23:25:58 +0100

    added new model (entity) JobEntry, added DateTimeHelper static class with ConvertToClarion method

---
+++ Helper/DateTimeHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace ProjectManagementApi.Helper
+{
+    public static class DateTimeHelper
+    {
+        public static (int, int) ConvertToClarion(DateTime dt)
+        {
+            DateTime datePart = dt.Date;
+            TimeSpan timePart = dt.TimeOfDay;
+            DateTime baseDate = new DateTime(1800, 12, 28);
+
+            (int, int) result = ((datePart - baseDate).Days, (int)(timePart.TotalSeconds * 100));
+        
+            return result;
+        }
+    }
+}
---
+++ Models/JobEntry.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjectManagementApi.Models
+{
+    public class JobEntry
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime Started { get; set; }
+        public DateTime Ended { get; set; }
+        public TimeSpan TimePassed { get; set; }
+    }
+}
