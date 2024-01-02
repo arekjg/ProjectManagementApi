7a0cf4b10e924bb32ddbc490347745cba8089b09
arekjg <arekjg@gmail.com>
2024-01-02 22:00:14 +0100

    moved the ProjectManagementApi project one directory up, added ProjectManagementApi.Tests project, added first unit test

---
+++ ProjectManagementApi.Tests/Controller/UserControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using ProjectManagementApi.Controllers;
+using ProjectManagementApi.Dtos;
+using ProjectManagementApi.Interfaces;
+
+namespace ProjectManagementApi.Tests.Controller
+{
+    public class UserControllerTests
+    {
+        private readonly IUserService _userService;
+        private readonly IMapper _mapper;
+        public UserControllerTests()
+        {
+            _userService = A.Fake<IUserService>();
+            _mapper = A.Fake<IMapper>();
+        }
+
+        [Fact]
+        public async Task UserController_GetUsers_ReturnOkAsync()
+        {
+            // Arrange
+            var users = A.Fake<ICollection<GetUserDto>>();
+            var usersList = A.Fake<List<GetUserDto>>();
+            A.CallTo(() => _mapper.Map<List<GetUserDto>>(users)).Returns(usersList);
+            var controller = new UserController(_userService);
+
+            // Act
+            var result = await controller.GetUsers();
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType(typeof(OkObjectResult));
+        }
+    }
+}
